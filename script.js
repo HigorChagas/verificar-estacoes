355eac16868b5bf1abf8dbf853ffbe05f9a418a7
Higor Chagas <higornchagas@gmail.com>
2021-09-22 17:11:36 -0300

    Refatorando o código

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ function verificarEstacoes() {
     
     const mes = prompt('Digite o mês por extenso (ex: Janeiro)');
     const resultado = document.querySelector('#resultado');
+    const img = document.createElement('img');
+    const addImg = document.body.appendChild(img);
+    
     
     let estacao = '';
     
-    imagensEstacoes(mes);
     addBackground(mes);
     
     switch(mes.toLowerCase())  {
         case 'janeiro': case 'fevereiro': case 'março':
             estacao = 'Inverno';
+            img.src = 'img/inverno.jpg';
             break;
         case 'abril': case 'maio': case 'junho':
             estacao = 'Primavera';
+            img.src = 'img/primavera.jpg';
             break;
         case 'julho': case 'agosto': case 'setembro':
             estacao = 'Verão';
+            img.src = 'img/verao.jpg'
             break;
         case 'outubro': case 'novembro': case 'dezembro':
             estacao = 'Outono';
+            img.src = 'img/outono.jpg'
             break;
         default:
             estacao = 'INDEFINIDA';
@@ -35,57 +41,15 @@ function addBackground(mes) {
 
     if(mes === 'janeiro' || mes === 'fevereiro' || mes === 'março') {
         container.style.backgroundColor = '#023e8a';
-        botao.style.backgroundColor = '#0096c7';
+        botao.classList.add('blue', 'btn-blue');
     } else if(mes === 'abril' || mes === 'maio' || mes === 'junho') {
         container.style.backgroundColor = '#e07be0';
-        botao.style.backgroundColor = '#ffafcc';
+        botao.classList.add('pink', 'btn-pink');
     } else if(mes === 'julho' || mes === 'agosto' || mes === 'setembro') {
         container.style.backgroundColor = '#dbb42c';
-        botao.style.backgroundColor = '#b08968';
+        botao.classList.add('brown', 'btn-brown');
     } else if(mes === 'outubro' || mes === 'novembro' || mes === 'dezembro') {
         container.style.backgroundColor = '#dc5318';
-        botao.style.backgroundColor = '#ae2012';
+        botao.classList.add('red', 'btn-red');
     }
 }
-
-
-function imagensEstacoes(mes) {
-    const img = document.createElement('img');
-    const addImg = document.body.appendChild(img);
-    const estacoesImg = [
-        img.src = 'img/inverno.jpg',
-        img.src = 'img/primavera.jpg',
-        img.src = 'img/verao.jpg',
-        img.src = 'img/outono.jpg'
-    ];
-
-    if(verificaEstacao(inverno)) {
-        addImg;
-        img.src = 'img/inverno.jpg';
-        console.log(addImg, estacoesImg);
-    }
-    
-}
-
-function verificaEstacao(inverno) {
-    inverno = 'janeiro' || 'fevereiro' || 'março';
-}
-
-function imagemPrimavera() {
-    img.src = 'img/primavera.jpg'
-    document.body.appendChild(img)
-    botao.classList.add('pink')
-}
-
-function imagemVerao() {
-    img.src = 'img/verao.jpg'
-    document.body.appendChild(img)
-    botao.classList.add('brown')
-}
-
-/* function imagemOutono() {
-    img.src = 'img/outono.jpg'
-    document.body.appendChild(img)
-    botao.classList.add('red')
-} */
-
